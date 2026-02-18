23c73e5ef0c3321eee9f331d655801d38df15da8
kyl3333 <kylee@s.sfusd.edu>
2026-02-18 11:20:12 -0800

    gotraycastworking
    yay

--- capstone-base-movement/enemy.gd
+++ capstone-base-movement/enemy.gd
@@ -2,49 +2,66 @@ extends CharacterBody2D
 @onready var player = get_node("/root/game/player/Legs")
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var timer = %Timer
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 @export var speed = 15000
 var tween = null
 var health = 100
-var in_detect_range = false
-var detecting = false
+var timer_not_active = true
+var detectable = false
 var detected = false
 var attack_distance = 200.0
 var recently_attacked = false
 var rotation_speed = PI
+var player_visible = false
 
 #start chase if u stay spotted too long lil bro
 func _on_timer_timeout() -> void:
-	detected = true
-	detecting = false
-	print("give chase")
-#first check if player entered fov
-#if they do then check if enemy has los w player
-#look at player if in detection radius with a tween
+	if player_visible:
+		detected = true
+		timer_not_active = false
+		detectable = false
+		print("give chase")
+
 func _on_detection_area_body_entered(_body: Node2D) -> void:
-	in_detect_range = true
-	var direction = (player.global_position - global_position).normalized()
-	var target_angle = direction.angle()
-	detecting = true
-	timer.start()
-	print("timer started, player detected")
-	if tween: 
-		tween.kill()
-	tween = create_tween()
-	tween.set_loops()
-	tween.tween_property(self,"rotation",target_angle, 0.75).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	detectable = true
+	timer_not_active = true
 
 #if player leaves dont do the stuff up there^
 func _on_detection_area_body_exited(_body: Node2D) -> void:
 	if tween:
 			tween.kill()
 	detected = false
-	detecting = false
-	in_detect_range = false
+	detectable = false
+	timer_not_active = true
 	timer.stop()
 	print("player got away :(")
 
 func _physics_process(delta: float):
-	#look at player while
+	#first check if player entered fov
+	#if they do then check if enemy has los w player
+	#look at player if in detection radius with a tween
+	if player_visible and detectable:
+			var direction = (player.global_position - global_position).normalized()
+			var target_angle = direction.angle()
+			if timer_not_active:
+				timer.start()
+				timer_not_active = false
+			if tween: 
+				tween.kill()
+			tween = create_tween()
+			tween.set_loops()
+			tween.tween_property(self,"rotation",target_angle, 0.75).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	#check line o sight
+	var player_local_position = ray_cast_2d.to_local(player.global_position)
+	ray_cast_2d.target_position = player_local_position
+	ray_cast_2d.force_raycast_update()
+	if ray_cast_2d.is_colliding():
+			var collider = ray_cast_2d.get_collider()
+			if collider == player:
+				player_visible = true
+			else:
+				player_visible = false
+				timer.stop()
 	#detect distance from player for attack
 	var distance_to_player: float = global_position.distance_to(player.global_position)
 	#if u get spotted
