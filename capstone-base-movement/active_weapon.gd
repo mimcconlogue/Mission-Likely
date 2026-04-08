d659d7b9535b9f3583b3a2521f10d6696e85379a
mimcconlogue <mimcconlogue@s.sfusd.edu>
2026-04-08 12:10:06 -0700

    new state machine for player animations

---
+++ capstone-base-movement/active_weapon.gd
@@ -0,0 +1,28 @@
+extends Node2D
+var grabbing = false
+func _input(event: InputEvent) -> void:
+	get_child(0).check_idle()
+	if Input.is_action_just_pressed("attack1"):
+		if get_child(0).current_animation_is_idle == true :
+			get_child(0).current_child.get_child(0).get_child(0).play("attack1")
+			get_child(0).current_animation_is_idle = false
+	if Input.is_action_just_pressed("attack2"):
+		if get_child(0).current_animation_is_idle == true :
+			get_child(0).current_child.get_child(0).get_child(0).play("attack2")
+			get_child(0).current_animation_is_idle = false
+	if Input.is_action_just_pressed("weaapon_up"):
+		if get_child(0).current_animation_is_idle == true :
+			get_child(0).current_child.get_child(0).get_child(0).play("special2")
+			get_child(0).current_animation_is_idle = false
+	if Input.is_action_just_pressed("weaapon_down"):
+		if get_child(0).current_animation_is_idle == true :
+			if get_child(0).prepared == false :
+				get_child(0).current_child.get_child(0).get_child(0).play("special")
+				get_child(0).current_animation_is_idle = false
+				grabbing = true
+
+
+func _on_weapon_animation_over() -> void:
+	if grabbing == true:
+		get_child(0).current_child.get_child(0).get_child(0).play("special_fail")
+		grabbing = false
