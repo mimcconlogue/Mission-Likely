30c278aeae68be701b4753336902faf67e1cc209
mimcconlogue <mimcconlogue@s.sfusd.edu>
2026-04-20 14:26:31 -0700

    fixed some errors and hopefully, again, fixed the no animation issue

--- capstone-base-movement/active_weapon.gd
+++ capstone-base-movement/active_weapon.gd
@@ -10,11 +10,11 @@ func _input(event: InputEvent) -> void:
 		if get_child(0).current_animation_is_idle == true :
 			get_child(0).current_child.get_child(0).get_child(0).play("attack2")
 			get_child(0).current_animation_is_idle = false
-	if Input.is_action_just_pressed("weaapon_up"):
+	if Input.is_action_just_pressed("weapon_up"):
 		if get_child(0).current_animation_is_idle == true :
 			get_child(0).current_child.get_child(0).get_child(0).play("special2")
 			get_child(0).current_animation_is_idle = false
-	if Input.is_action_just_pressed("weaapon_down"):
+	if Input.is_action_just_pressed("weapon_down"):
 		if get_child(0).current_animation_is_idle == true :
 			if get_child(0).prepared == false :
 				get_child(0).current_child.get_child(0).get_child(0).play("special")
