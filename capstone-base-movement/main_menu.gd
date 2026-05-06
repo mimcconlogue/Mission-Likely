1ccdea43e90e81b4cc8c9ad68e5ab2fa7d1a44a2
kyl3333 <kylee@s.sfusd.edu>
2026-05-06 10:46:45 -0700

    more ui stuff

--- capstone-base-movement/main_menu.gd
+++ capstone-base-movement/main_menu.gd
@@ -1,7 +1,14 @@
 extends Control
+@onready var color_rect: ColorRect = $ColorRect
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-
+func _ready() -> void:
+	animation_player.play("RESET")
+	color_rect.visible = true
+	animation_player.play("fade_to_clear")
 func _on_start_button_pressed() -> void:
+	animation_player.play("fade_to_black")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_exit_button_pressed() -> void:
